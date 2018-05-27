c84e9f0fee50900de2e89d852f52b48887ad6c89
TempestTraxx <alir.htc@gmail.com>
2018-05-28 00:12:31 +0200

    NM

--- jsmain.js
+++ jsmain.js
@@ -1,10 +1,20 @@
-var request= new XMLHttpRequest();
-request.open('GET', 'https://raw.githubusercontent.com/TempestTraxx/teplomer/master/data.json');
-request.onload = function () {
-    //console.log(request.responseText)
-var data=JSON.parse(request.responseText);
-console.log(JSON.parse(data[0]));
+
+var tlac = document.getElementById("tl");
+
+tlac.addEventListener("click", function () {
+
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://raw.githubusercontent.com/TempestTraxx/teplomer/master/data.json');
+    request.onload = function () {
+        //console.log(request.responseText)
+        var data = JSON.parse(request.responseText);
+        console.log(JSON.parse(data[0]));
+    };
+    request.send();
+});
 
 
-};
-request.send();
+function vyber() {
+    var date = document.getElementById("datumy");
+    var dateString = date.options[date.selectedIndex].text;
+    document.getElementById("datum").innerHTML = dateString;}
